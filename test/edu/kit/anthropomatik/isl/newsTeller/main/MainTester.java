8defcbedffafab02a6f66eb00a8e7424679fed90
ukcvo <ukcvo@student.kit.edu>
2015-10-20 13:36:47 +0200

    refactored MainTester (do initialization in separate function)

--- test/edu/kit/anthropomatik/isl/newsTeller/main/MainTester.java
+++ test/edu/kit/anthropomatik/isl/newsTeller/main/MainTester.java
@@ -2,6 +2,7 @@ package edu.kit.anthropomatik.isl.newsTeller.main;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
@@ -9,12 +10,17 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class MainTester {
 
+	private Main m;
+	
+	@Before
+	public void init() {
+		ApplicationContext context = new FileSystemXmlApplicationContext("config/Scope0_test.xml");
+		m = (Main) context.getBean("main");
+		((AbstractApplicationContext) context).close();
+	}
+	
 	@Test
 	public void shouldReturnBeep() {
-		ApplicationContext context = new FileSystemXmlApplicationContext("config/Scope0_test.xml");
-		Main m = (Main) context.getBean("main");
-		((AbstractApplicationContext) context).close();
-		
 		assertTrue(m.getMsg().equals("beep"));
 	}
 
