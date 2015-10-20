20e498aa82cac2d0467b22999de21c79de18dd1a
ukcvo <ukcvo@student.kit.edu>
2015-10-20 15:57:00 +0200

    checking if logging works

--- src/edu/kit/anthropomatik/isl/newsTeller/main/Main.java
+++ src/edu/kit/anthropomatik/isl/newsTeller/main/Main.java
@@ -1,5 +1,7 @@
 package edu.kit.anthropomatik.isl.newsTeller.main;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
@@ -23,10 +25,15 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		Log log = LogFactory.getLog(Main.class);
+		log.info("starting");
+		
 		ApplicationContext context = new FileSystemXmlApplicationContext("config/Scope0.xml");
 		Main m = (Main) context.getBean("main");
 		((AbstractApplicationContext) context).close();
+		log.debug("about to beep");
 		m.beep();
+		log.warn("shutting down!");
 		
 	}
 
