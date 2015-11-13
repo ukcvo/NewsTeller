8b349a7c409a4f0c818ed0eabd748eae658a4be2
ukcvo <ukcvo@student.kit.edu>
2015-11-13 16:45:36 +0100

    added python script for removing timestamp lines from log

---
+++ removeLogTimestamps.py
@@ -0,0 +1,9 @@
+import sys
+
+inputFileName = sys.argv[1]
+outputFileName = sys.argv[2]
+
+with open(inputFileName, 'r') as f, open(outputFileName, 'w') as o:
+    for line in f:
+        if "anthropomatik" not in line:
+            o.write(line)
