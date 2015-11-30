a47b02bc1795e98e9d999a27d614e30e125ff156
ukcvo <ukcvo@student.kit.edu>
2015-11-30 16:16:55 +0100

    refactored DBPediaLabelInFullTextFeature in order to look only at entities that originally matched the keyword

--- src/edu/kit/anthropomatik/isl/newsTeller/retrieval/filtering/features/DBPediaLabelInFullTextFeature.java
+++ src/edu/kit/anthropomatik/isl/newsTeller/retrieval/filtering/features/DBPediaLabelInFullTextFeature.java
@@ -6,7 +6,8 @@ import edu.kit.anthropomatik.isl.newsTeller.data.Keyword;
 import edu.kit.anthropomatik.isl.newsTeller.util.Util;
 
 /**
- * Checks if all entities are actually mentioned somewhere in the text with one of their DBPedia labels.
+ * Checks if entities are actually mentioned somewhere in the text with one of their DBPedia labels.
+ * Either look at all entities or only at the ones matching the keyword (depending on sparql query).
  * 
  * @author Lucas Bechberger (ukcvo@student.kit.edu, bechberger@fbk.eu)
  *
@@ -24,12 +25,16 @@ public class DBPediaLabelInFullTextFeature extends FullTextFeature {
 	protected List<List<String>> getLabels(String eventURI, List<Keyword> keywords) {
 		List<List<String>> result = new ArrayList<List<String>>();
 		
-		List<String> entities = ksAdapter.runSingleVariableStringQuery(sparqlQuery.replace(Util.PLACEHOLDER_EVENT, eventURI), Util.VARIABLE_ENTITY);
-		
-		for (String entity : entities) {
-			List<String> labels = ksAdapter.runSingleVariableStringQuery(labelQuery.replace(Util.PLACEHOLDER_ENTITY, entity), Util.VARIABLE_LABEL);
-			result.add(labels);
+		for (Keyword keyword : keywords) {
+			List<String> entities = ksAdapter.runSingleVariableStringQuery(
+					sparqlQuery.replace(Util.PLACEHOLDER_EVENT, eventURI).replace(Util.PLACEHOLDER_KEYWORD, keyword.getStemmedRegex()), Util.VARIABLE_ENTITY);
+			
+			for (String entity : entities) {
+				List<String> labels = ksAdapter.runSingleVariableStringQuery(labelQuery.replace(Util.PLACEHOLDER_ENTITY, entity), Util.VARIABLE_LABEL);
+				result.add(labels);
+			}
 		}
+		
 		
 		return result;
 	}
--- test/edu/kit/anthropomatik/isl/newsTeller/retrieval/filtering/features/DBPediaLabelInFullTextFeatureTest.java
+++ test/edu/kit/anthropomatik/isl/newsTeller/retrieval/filtering/features/DBPediaLabelInFullTextFeatureTest.java
@@ -2,6 +2,8 @@ package edu.kit.anthropomatik.isl.newsTeller.retrieval.filtering.features;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.LogManager;
 
 import org.junit.After;
@@ -12,11 +14,14 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
+import edu.kit.anthropomatik.isl.newsTeller.data.Keyword;
 import edu.kit.anthropomatik.isl.newsTeller.knowledgeStore.KnowledgeStoreAdapter;
+import edu.kit.anthropomatik.isl.newsTeller.util.Util;
 
 public class DBPediaLabelInFullTextFeatureTest {
 
 	private DBPediaLabelInFullTextFeature feature;
+	private DBPediaLabelInFullTextFeature keywordFeature;
 	private KnowledgeStoreAdapter ksAdapter;
 	
 	@BeforeClass
@@ -33,6 +38,7 @@ public class DBPediaLabelInFullTextFeatureTest {
 	public void setUp() throws Exception {
 		ApplicationContext context = new FileSystemXmlApplicationContext("config/test.xml");
 		feature = (DBPediaLabelInFullTextFeature) context.getBean("appearLabelsInTextFeature");
+		keywordFeature = (DBPediaLabelInFullTextFeature) context.getBean("appearKeywordLabelsInTextFeature");
 		ksAdapter = (KnowledgeStoreAdapter) context.getBean("ksAdapter");
 		((AbstractApplicationContext) context).close();
 		ksAdapter.openConnection();
@@ -45,14 +51,46 @@ public class DBPediaLabelInFullTextFeatureTest {
 
 	@Test
 	public void ShouldReturnZero() {
-		double value = feature.getValue("http://en.wikinews.org/wiki/Autopsy_reveals_that_Terri_Schiavo_was_in_a_persistent_vegetative_state#ev17", null);
+		Keyword k = new Keyword("Michael Jackson");
+		Util.stemKeyword(k);
+		List<Keyword> keywords = new ArrayList<Keyword>();
+		keywords.add(k);
+		
+		double value = feature.getValue("http://en.wikinews.org/wiki/Autopsy_reveals_that_Terri_Schiavo_was_in_a_persistent_vegetative_state#ev17", keywords);
 		assertTrue(value == 0.0);
 	}
 	
 	@Test
 	public void ShouldReturnOne() {
-		double value = feature.getValue("http://en.wikinews.org/wiki/Walter_Frederick_Morrison,_inventor_of_frisbee,_dies_at_age_90#ev18_3", null);
+		Keyword k = new Keyword("Flying disc");
+		Util.stemKeyword(k);
+		List<Keyword> keywords = new ArrayList<Keyword>();
+		keywords.add(k);
+		
+		double value = feature.getValue("http://en.wikinews.org/wiki/Walter_Frederick_Morrison,_inventor_of_frisbee,_dies_at_age_90#ev18_3", keywords);
 		assertTrue(value == 1.0);
 	}
 
+	@Test
+	public void ShouldReturnTwoThirds() {
+		Keyword k = new Keyword("comedy");
+		Util.stemKeyword(k);
+		List<Keyword> keywords = new ArrayList<Keyword>();
+		keywords.add(k);
+		
+		double value = feature.getValue("http://en.wikinews.org/wiki/US_nuclear_security_director_asked_to_resign#ev11", keywords);
+		assertTrue(Math.abs(value - (2.0/3)) < Util.EPSILON);
+	}
+	
+	@Test
+	public void ShouldReturnZeroForKeyword() {
+		Keyword k = new Keyword("comedy");
+		Util.stemKeyword(k);
+		List<Keyword> keywords = new ArrayList<Keyword>();
+		keywords.add(k);
+		
+		double value = keywordFeature.getValue("http://en.wikinews.org/wiki/US_nuclear_security_director_asked_to_resign#ev11", keywords);
+		assertTrue(value == 0.0);
+	}
+	
 }
