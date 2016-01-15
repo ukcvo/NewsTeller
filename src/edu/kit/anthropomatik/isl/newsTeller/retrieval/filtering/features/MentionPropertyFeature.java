ca0f60fae844f6fa02819a31a1071e918a2142d5
ukcvo <ukcvo@student.kit.edu>
2016-01-15 17:00:35 +0100

    added MentionPropertyFeature

---
+++ src/edu/kit/anthropomatik/isl/newsTeller/retrieval/filtering/features/MentionPropertyFeature.java
@@ -0,0 +1,40 @@
+package edu.kit.anthropomatik.isl.newsTeller.retrieval.filtering.features;
+
+import java.util.List;
+
+import edu.kit.anthropomatik.isl.newsTeller.data.Keyword;
+import edu.kit.anthropomatik.isl.newsTeller.util.Util;
+
+/**
+ * Counts average number of results for a given property.
+ * 
+ * @author Lucas Bechberger (ukcvo@student.kit.edu, bechberger@fbk.eu)
+ *
+ */
+public class MentionPropertyFeature extends UsabilityFeature {
+
+	private String propertyURI;
+	
+	public MentionPropertyFeature(String queryFileName, String propertyURI) {
+		super(queryFileName);
+		this.propertyURI = propertyURI;
+	}
+
+	@Override
+	public double getValue(String eventURI, List<Keyword> keywords) {
+		
+		double result = 0;
+		
+		List<String> mentionURIs = ksAdapter.runSingleVariableStringQuery(sparqlQuery.replace(Util.PLACEHOLDER_EVENT, eventURI), 
+				Util.VARIABLE_MENTION, false);
+		
+		for (String mentionURI : mentionURIs) {
+			List<String> propertyValues = ksAdapter.getMentionProperty(mentionURI, propertyURI); //TODO: use Record.count instead? --> faster?
+			result += propertyValues.size();
+		}
+		result = result / mentionURIs.size();
+		
+		return result;
+	}
+
+}
