9a99c080e3aa825dcf08cc1600e75e861783fe22
ukcvo <ukcvo@student.kit.edu>
2016-02-08 13:16:06 +0100

    added SentenceContainsCharactersFeature

---
+++ src/edu/kit/anthropomatik/isl/newsTeller/retrieval/ranking/features/SentenceContainsCharactersFeature.java
@@ -0,0 +1,38 @@
+package edu.kit.anthropomatik.isl.newsTeller.retrieval.ranking.features;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import edu.kit.anthropomatik.isl.newsTeller.data.Keyword;
+import edu.kit.anthropomatik.isl.newsTeller.userModel.UserModel;
+
+public class SentenceContainsCharactersFeature extends RankingFeature {
+
+	private List<String> chars;
+	
+	public void setChars(List<String> chars) {
+		this.chars = chars;
+	}
+	
+	@Override
+	public double getValue(String eventURI, List<Keyword> keywords, UserModel userModel) {
+		
+		double result = 0.0;
+		
+		Set<String> sentences = new HashSet<String>(ksAdapter.retrieveSentencesfromEvent(eventURI, keywords.get(0).getWord()));
+		
+		for (String sentence : sentences) {
+			double counter = 0.0;
+			for (String c : this.chars) {
+				if (sentence.contains(c))
+					counter++;
+			}
+			
+			result = Math.max(result, counter);
+		}
+		
+		return result;
+	}
+
+}
---
+++ test/edu/kit/anthropomatik/isl/newsTeller/retrieval/ranking/features/SentenceContainsCharactersFeatureTest.java
@@ -0,0 +1,87 @@
+package edu.kit.anthropomatik.isl.newsTeller.retrieval.ranking.features;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.logging.LogManager;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import com.google.common.collect.Sets;
+
+import edu.kit.anthropomatik.isl.newsTeller.data.KSMention;
+import edu.kit.anthropomatik.isl.newsTeller.data.Keyword;
+import edu.kit.anthropomatik.isl.newsTeller.knowledgeStore.KnowledgeStoreAdapter;
+import edu.kit.anthropomatik.isl.newsTeller.userModel.DummyUserModel;
+import edu.kit.anthropomatik.isl.newsTeller.userModel.UserModel;
+import edu.kit.anthropomatik.isl.newsTeller.util.Util;
+
+public class SentenceContainsCharactersFeatureTest {
+
+private KnowledgeStoreAdapter ksAdapter;
+	
+	private SentenceContainsCharactersFeature feature;
+	
+	
+	private static ConcurrentMap<String, ConcurrentMap<String, Set<String>>> sparqlCache = new ConcurrentHashMap<String, ConcurrentMap<String, Set<String>>>();
+	private static ConcurrentMap<String, Set<KSMention>> eventMentionCache = new ConcurrentHashMap<String, Set<KSMention>>();
+	private static List<Keyword> keywords = new ArrayList<Keyword>();
+	private static UserModel userModel = new DummyUserModel();
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.setProperty("java.util.logging.config.file", "./config/logging-test.properties");
+		try {
+			LogManager.getLogManager().readConfiguration();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		ConcurrentMap<String, Set<String>> eventMentionMap = new ConcurrentHashMap<String, Set<String>>();
+		eventMentionMap.put("event-1", Sets.newHashSet("mention-1#char=4,7"));
+		eventMentionMap.put("event-2", Sets.newHashSet("mention-1#char=4,7", "mention-2#char=4,7"));
+		
+		ConcurrentMap<String, Set<String>> resourceTextMap = new ConcurrentHashMap<String, Set<String>>();
+		resourceTextMap.put("mention-1", Sets.newHashSet("This does not contain any quotation marks."));
+		resourceTextMap.put("mention-2", Sets.newHashSet("This sentence \" however does."));
+		
+		
+		sparqlCache.put(Util.getRelationName("event", "mention", "keyword"), eventMentionMap);
+		sparqlCache.put(Util.RELATION_NAME_RESOURCE_TEXT, resourceTextMap);
+		
+		
+		Keyword k = new Keyword("keyword");
+		Util.stemKeyword(k);
+		keywords.add(k);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		ApplicationContext context = new FileSystemXmlApplicationContext("config/test.xml");
+		feature = (SentenceContainsCharactersFeature) context.getBean("sentenceContainsQuotationFeature");
+		ksAdapter = (KnowledgeStoreAdapter) context.getBean("ksAdapter");
+		((AbstractApplicationContext) context).close();
+		ksAdapter.manuallyFillCaches(sparqlCache, eventMentionCache);
+	}
+
+	@Test
+	public void shouldReturnZero() {
+		double value = feature.getValue("event-1", keywords, userModel);
+		assertTrue(value == 0.0);
+	}
+
+	@Test
+	public void shouldReturnOne() {
+		double value = feature.getValue("event-2", keywords, userModel);
+		assertTrue(value == 1.0);
+	}
+}
