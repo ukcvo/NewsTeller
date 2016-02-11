a82c6c6aa568e4ed3c135949162617c717e506f0
ukcvo <ukcvo@student.kit.edu>
2016-02-11 15:11:04 +0100

    improved RankingExamplesGenerator, created data for usr_01

--- src/edu/kit/anthropomatik/isl/newsTeller/tools/RankingExamplesGenerator.java
+++ src/edu/kit/anthropomatik/isl/newsTeller/tools/RankingExamplesGenerator.java
@@ -14,6 +14,7 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.util.StringUtils;
 
 import edu.kit.anthropomatik.isl.newsTeller.data.Keyword;
 import edu.kit.anthropomatik.isl.newsTeller.data.NewsEvent;
@@ -83,22 +84,61 @@ public class RankingExamplesGenerator {
 		this.userID = userID;
 	}
 
+	private class EventSentenceTuple {
+		
+		private NewsEvent event;
+		private String sentence;
+		
+		public NewsEvent getEvent() {
+			return event;
+		}
+		
+		public String getSentence() {
+			return sentence;
+		}
+		
+		public EventSentenceTuple(NewsEvent event, String sentence) {
+			this.event = event;
+			this.sentence = sentence;
+		}
+	}
+	
+	private class QueryResultTuple implements Comparable<QueryResultTuple> {
+		
+		private List<String> query;
+		private List<EventSentenceTuple> result;
+		
+		public List<String> getQuery() {
+			return this.query;
+		}
+		
+		public List<EventSentenceTuple> getResult() {
+			return this.result;
+		}
+		
+		public QueryResultTuple(List<String> query, List<EventSentenceTuple> result) {
+			this.query = query;
+			this.result = result;
+		}
+
+		@Override
+		public int compareTo(QueryResultTuple arg0) {
+			return Integer.compare(this.result.size(), arg0.getResult().size());
+		}
+	}
+	
 	public void run() {
 		ksAdapter.openConnection();
 		
 		UserModel dummyUserModel = new DummyUserModel();
-		
-		int numberOfEventsPerQuery = (int) Math.ceil(totalNumberOfEvents / queries.size());
-		int numberOfEventsForNextQuery = numberOfEventsPerQuery;
-		int numberOfAlreadyCollectedEvents = 0;
-		int i = 0;
-		
+
 		Map<String, List<String>> fileNameToKeywordMap = new HashMap<String, List<String>>();
 		
+		List<QueryResultTuple> queryResultList = new ArrayList<QueryResultTuple>();
+		
 		for (List<String> query : this.queries) {
-			if (numberOfAlreadyCollectedEvents >= totalNumberOfEvents)
-				break;
-			
+			if (log.isInfoEnabled())
+				log.info(StringUtils.collectionToCommaDelimitedString(query));
 			List<Keyword> queryKeywords = new ArrayList<Keyword>();
 			for (String word : query) 
 				queryKeywords.add(new Keyword(word));
@@ -106,37 +146,56 @@ public class RankingExamplesGenerator {
 			Set<NewsEvent> events = searcher.findEvents(queryKeywords, dummyUserModel);
 			Set<NewsEvent> filteredEvents = filter.filterEvents(events, queryKeywords);
 			List<NewsEvent> rankedEvents = ranker.rankEvents(filteredEvents, queryKeywords, dummyUserModel);
-			
-			List<NewsEvent> selectedEvents = rankedEvents.subList(0, Math.min(numberOfEventsForNextQuery, rankedEvents.size()));
-			if (selectedEvents.size() == numberOfEventsForNextQuery)
-				numberOfEventsForNextQuery = numberOfEventsPerQuery;
-			else
-				numberOfEventsForNextQuery = numberOfEventsPerQuery + (numberOfEventsForNextQuery - selectedEvents.size());
-			numberOfAlreadyCollectedEvents += selectedEvents.size();
-			
-			String folderName = String.format("%s/%s", outputFolder, userID);
-			File folder = new File(folderName);
-			if (!folder.exists())
-				folder.mkdirs();
-			
-			String queryFileName = String.format("%s/query_%d.csv", folderName, i);
-			Map<String, String> eventToSentenceMap = new HashMap<String, String>();
 
-			for (NewsEvent event : selectedEvents) {
+			List<EventSentenceTuple> sentenceTuples = new ArrayList<EventSentenceTuple>();
+			for (NewsEvent event : rankedEvents) {
 				String eventURI = event.getEventURI();
 				List<String> sentences = ksAdapter.retrieveSentencesFromEvent(eventURI, query.get(0));
 				Collections.shuffle(sentences);
 				String sentence = sentences.get(0);
+				sentenceTuples.add(new EventSentenceTuple(event, sentence));
+			}
+			
+			if (!rankedEvents.isEmpty())
+				queryResultList.add(new QueryResultTuple(query, sentenceTuples));
+		}
+		
+		Collections.sort(queryResultList);
+		
+		String folderName = String.format("%s/%s", outputFolder, userID);
+		File folder = new File(folderName);
+		if (!folder.exists())
+			folder.mkdirs();
+
+		
+		int numberOfEventsStillMissing = this.totalNumberOfEvents;
+				
+		for (int i = 0; (i < queryResultList.size()) && (numberOfEventsStillMissing > 0); i++) {
+			List<String> query = queryResultList.get(i).getQuery();
+			List<EventSentenceTuple> events = queryResultList.get(i).getResult();
+			
+			int numberOfEventToTake = Math.min(events.size(), (int) Math.ceil(numberOfEventsStillMissing / (queryResultList.size() - i)));
+			List<EventSentenceTuple> selectedEvents = events.subList(0, numberOfEventToTake);
+			numberOfEventsStillMissing = numberOfEventsStillMissing - numberOfEventToTake;
+			
+			String queryFileName = String.format("%s/query_%d.csv", folderName, i);
+			Map<String, String> eventToSentenceMap = new HashMap<String, String>();
+
+			for (EventSentenceTuple event : selectedEvents) {
+				String eventURI = event.getEvent().getEventURI();
+				String sentence = event.getSentence();
 				eventToSentenceMap.put(eventURI, sentence);
 			}
 			
 			Util.writeQueryFile(queryFileName, eventToSentenceMap);
 			
 			fileNameToKeywordMap.put(queryFileName, query);
-			i++;
 			if (log.isInfoEnabled())
-				log.info(queryFileName);
+				log.info(String.format("%s: %d events", queryFileName, numberOfEventToTake));
 		}
+		
+		if ((numberOfEventsStillMissing > 0) && log.isWarnEnabled())
+			log.warn(String.format("Still missing %d events!", numberOfEventsStillMissing));
 		
 		Util.writeUserConfigFile(String.format("%s/%s.csv", outputFolder, userID), interests, fileNameToKeywordMap);
 		
